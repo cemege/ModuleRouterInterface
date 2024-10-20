fd0dd8b7f874b1b3f57b187de2a9cf7b00a15983
Cem Ege <cemege.kilincoglu@arabam.com>
2024-10-20 15:27:03 +0300

    Initial Commit

---
+++ ModuleInterface/ModuleInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModuleInterfaceApp.swift
+//  ModuleInterface
+//
+//  Created by cemegeArabam on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ModuleInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModuleInterfaceTests/ModuleInterfaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  ModuleInterfaceTests.swift
+//  ModuleInterfaceTests
+//
+//  Created by cemegeArabam on 20.10.2024.
+//
+
+import Testing
+@testable import ModuleInterface
+
+struct ModuleInterfaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
